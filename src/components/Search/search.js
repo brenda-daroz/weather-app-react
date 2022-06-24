c2a9bc907b86473ba11339dd9c480b6ebcefbeb9
brenda-daroz <brenda.daroz@gmail.com>
2022-06-24 11:39:28 +0200

    Create components folder

---
+++ src/components/Search/search.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import { requestByLocation } from "../../openWeatherMap";
+
+
+
+export function Search (props) {
+    const [location, setLocation] = useState('')
+
+
+    const searchLocation = (event) => {
+        
+    if (event.key === "Enter") {
+      requestByLocation.then(props.fn)
+      setLocation('')
+    }
+  }
+    return (
+        
+        <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter location"
+          type="text" />
+      </div>
+    )
+}
