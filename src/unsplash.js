74717900468c0d5d142f743fb245d0c35d07284b
brenda-daroz <brenda.daroz@gmail.com>
2022-06-30 13:10:47 +0200

    Refactor function

--- src/unsplash.js
+++ src/unsplash.js
@@ -3,33 +3,31 @@ import { createApi } from 'unsplash-js';
 
 
 export default function ChangeImage(props) {
-  
+
   const unsplash = createApi({
-      accessKey: "Vo_E9LCr3ED3sQw28VBO0y_FK9Zbaxz2erl1oV_WisY"
-    });
+    accessKey: "Vo_E9LCr3ED3sQw28VBO0y_FK9Zbaxz2erl1oV_WisY"
+  });
 
-  
-const [figure, setImage] = useState(null)
+  const [figure, setImage] = useState(null)
 
+  function grabImage(response) {
+    const data = response.response.results
+    const randomData = data[Math.floor(Math.random())]
+    const image = randomData.urls.regular
+    setImage(image)
+  }
 
-useEffect( () => {
-  unsplash.search.getPhotos({
-  query: props.image,
-  page: 1,
-  perPage: 10,
-  orientation: 'portrait',
-}).then(response => {
-  const data = response.response.results
-  const randomData = data[Math.floor(Math.random())]
-  const image = randomData.urls.regular
-  setImage(image)
-  
-})}
+  useEffect(() => {
+    unsplash.search.getPhotos({
+      query: props.image,
+      page: 1,
+      perPage: 10,
+      orientation: 'portrait',
+    }).then(grabImage)
+      .catch(error => console.log(error))
+  })
 
-)
-
-return (
-  <img src={figure} alt='' />
-)
-
+  return (<
+    img src={figure} alt='' />
+  )
 }
