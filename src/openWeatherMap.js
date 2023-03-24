2da72f90897f909e1e5003e9c4b0bb3980dcb620
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-24 16:21:22 +0100

    hide key

--- src/openWeatherMap.js
+++ src/openWeatherMap.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
 
-const API_KEY = "1773caa99e424b652cf227671991b8a2"
+// const API_KEY = "1773caa99e424b652cf227671991b8a2"
 const API_HOST = "api.openweathermap.org"
 
 function requestByCoordinates(lat, lon) {
-    return axios.get(`https://${API_HOST}/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
-  }
-
-function requestByLocation(location) {
-    return axios.get(`https://${API_HOST}/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`)
+  return axios.get(`https://${API_HOST}/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
 }
 
-export {requestByCoordinates, requestByLocation}
+function requestByLocation(location) {
+  return axios.get(`https://${API_HOST}/data/2.5/weather?q=${location}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+}
+
+export { requestByCoordinates, requestByLocation }
